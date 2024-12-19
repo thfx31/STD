a5f8cec5595fb814758054a094f4a42552a3255c
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2024-12-19 14:28:55 +0100

    feat: ajouter un workflow de construction et de publication Docker, supprimer les ressources ECR

--- terraform/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "std_chat" {
-  name                 = "std-chat"
-  image_tag_mutability = "MUTABLE"
-}
--- terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.std_chat.repository_url
-}
