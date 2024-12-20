8ec32e1b9e9ffa5283c98754f7c1dcdfb7b89e89
Dylan <dylan31400@outlook.com>
2024-12-20 10:22:28 +0100

    ajout variables

--- terraform/test.tf
+++ terraform/test.tf
@@ -140,3 +140,36 @@ resource "aws_lb_listener" "TP_Listener" {
     target_group_arn = aws_lb_target_group.TP_TargetGroup.arn 
   }
 }
+
+variable "region" {
+  description = "Région AWS où déployer les ressources"
+  type        = string
+  default     = "eu-west-1" 
+}
+
+
+variable "instance_type" {
+  description = "Type d'instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+
+variable "vpc_id" {
+  description = "ID du VPC où déployer les ressources"
+  type        = string
+  default     = "vpc-0035b5ae8bbbefd3f"
+}
+
+variable "subnets" {
+  description = "Liste des sous-réseaux pour les instances et le Load Balancer"
+  type        = list(string)
+  default = ["subnet-02ae3d0545ef9967e",
+  "subnet-01bac5268bd103c55"]
+}
+
+variable "security_group" {
+  description = "ID du Security Group à utiliser"
+  type        = string
+  default     = "sg-0b92c45c5cd41a041"
+}
