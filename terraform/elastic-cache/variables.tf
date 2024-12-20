ed69dccdaa0aa9610f187cbafb431f655fc0faf9
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2024-12-20 12:22:18 +0100

    fix: load balancer stickiness

--- terraform/elastic-cache/variables.tf
+++ terraform/elastic-cache/variables.tf
@@ -2,3 +2,8 @@ variable "ec2_security_group_id" {
   description = "The ID of the EC2 security group."
   type        = string
 }
+
+variable "vpc_id" {
+  description = "The ID of the VPC."
+  type        = string
+}
