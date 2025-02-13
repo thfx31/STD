38d10700307cfa7fa1bb8afc55719c4566cbd2fa
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2025-02-13 14:37:45 +0100

    refactor: supprimer les variables inutilisées dans les fichiers Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,12 +4,6 @@ variable "region" {
   default     = "eu-west-1"
 }
 
-variable "instance_type" {
-  description = "Type d'instance EC2"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "vpc_id" {
   description = "ID du VPC où déployer les ressources"
   type        = string
@@ -21,10 +15,4 @@ variable "public_subnets" {
   type        = list(string)
   default = ["subnet-02ae3d0545ef9967e",
   "subnet-01bac5268bd103c55"]
-}
-
-variable "security_group" {
-  description = "ID du Security Group à utiliser"
-  type        = string
-  default     = "sg-0b92c45c5cd41a041"
 }
